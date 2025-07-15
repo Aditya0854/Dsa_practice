3caf181733c21fbd0f4c07d65146451bf934d57f
Aditya0854 <adityaashu826@gmail.com>
2025-07-16 00:13:21 +0530

    Add two numbers denoted by two linked lists and print their result as a  linked list

---
+++ LinkedList/Medium/addTwoNumbers.java
@@ -0,0 +1,57 @@
+package LinkedList.Medium;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class addTwoNumbers {
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        int carry = 0;
+
+        while(l1 != null || l2 != null || carry != 0 ) {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+            
+            int sum = val1 + val2 + carry;
+
+            carry = sum / 10;
+
+            current.next  = new ListNode(sum % 10);
+            current = current.next;
+
+            if( l1 != null) l1 = l1.next;
+            if( l2 != null) l2 = l2.next;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+        ListNode result = addTwoNumbers(l1, l2);
+
+        // Print the result
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+        
+    }
+}
