a6dba3a64353df9873089ce6c43f427130eb9131
Aditya0854 <adityaashu826@gmail.com>
2025-07-31 19:53:22 +0530

    AVL trees

---
+++ Trees/AVL/AvlTrees.java
@@ -0,0 +1,19 @@
+// package Trees.AVL;
+
+class AVL {
+    class Node {
+        int key, height;
+        Node left, right;
+
+        Node(int d) {
+            key = d;
+            height = 1;
+        }
+    }
+}
+
+public class AvlTrees {
+    public static void main(String[] args) {
+        
+    }
+}
