cc5b339b81bcd5f926cc49c853f993dda384b2ba
Aditya0854 <adityaashu826@gmail.com>
2025-08-06 00:29:36 +0530

    Activity Selection problem implementation

---
+++ Greedy/ActivitySelection.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class ActivitySelection {
+    static class Activity {
+        int start, end;
+
+        Activity(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    public static int maxActivities(int[] start, int[] end) {
+        int n = start.length;
+        Activity[] activities = new Activity[n];
+        for(int i = 0; i< n; i++) {
+            activities[i] = new Activity(start[i], end[i]);
+        }
+
+        Arrays.sort(activities, (a,b) -> a.end-b.end);
+
+        int count = 1;
+        int lastEnd = activities[0].end;
+
+        for(int i = 1; i< n; i++) {
+            if(activities[i].start >=lastEnd) {
+                count++;
+                lastEnd = activities[i].end;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] start = {1, 3, 0, 5, 8, 5};
+        int[] end = {2, 4, 6, 7, 9, 9};
+        int result = maxActivities(start, end);
+        System.out.println("Maximum number of activities: " + result);
+    }
+}
