3ca60590189ed3a1f13d1335807506033cac5dc6
Aditya0854 <adityaashu826@gmail.com>
2025-08-12 12:26:40 +0530

    Lemonade Change solution for handling change in a lemonade stand

---
+++ Greedy/LemonadeChange.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class LemonadeChange {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+
+        for(int bill : bills) {
+            if(bill == 5){
+                five++;
+            }
+            else if(bill == 10) {
+                if(five == 0) {
+                    return false;
+                } else {
+                    five--;
+                    ten++;
+                }
+            } else {
+                if(ten > 0 && five > 0){
+                    five--;
+                    ten--;
+                } else if(five >=3){
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        LemonadeChange lemonadeChange = new LemonadeChange();
+        int[] bills = {5, 5, 10, 10, 20};
+        System.out.println(lemonadeChange.lemonadeChange(bills)); // Output: false
+    }
+}
